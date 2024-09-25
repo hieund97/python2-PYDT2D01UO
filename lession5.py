46a7a5555e1ac6cfc8e5e9387f68c3a28b1a54d5
Nguyen Duc Hieu <hieund@geniee.co.jp>
2024-09-25 20:53:47 +0700

    commit lession5

---
+++ lession5.py
@@ -0,0 +1,50 @@
+"""
+
+integer
+float
+string
+boolen
+list  []
+tuple ()
+dictionary {}
+
+
+"""
+
+# dict = {
+#     "name": "Trường",
+#     "tuoi": 15,
+#     "diachi": "Ha Noi",
+#     "gioi_tinh": True
+# }
+
+# set1 = {15,16,17, "hello"}
+# set2 = {15,16,17,"hello1"}
+
+# dict.clear()
+# Bạn trường có điểm toán là 9 điểm
+# print(dict.values())
+# print(dict)
+# print(set1.issubset(set2))
+
+
+# lập trình kiểm tra từ có nằm trong file sample.txt hay khong
+# Tạo 1 input nhập từ
+# kiểm tra từ đó có trong file hay không
+
+
+def check_word(word):
+    with open("sample.txt", "r") as f:
+        noidung = f.read()
+        text = noidung.split() # phân tách các chữ theo 1 ký tự, mặc định là dấu cách
+        for i in text:
+            if i == word:
+                return True
+        return False
+    
+word = input("Nhập từ kiểm tra: ")
+
+if check_word(word):
+    print(f"{word} có nằm trong file")
+else:
+    print(f"{word} không nằm trong file")
